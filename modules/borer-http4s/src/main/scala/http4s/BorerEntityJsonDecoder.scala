e19400021c2962d8ff7a1ef242b4e5540d7fac21
Eike <eikek@posteo.de>
2025-03-07 07:49:14 +0100

    Replace async constraint with sync in http4s entity codec

--- modules/borer-http4s/src/main/scala/http4s/BorerEntityJsonDecoder.scala
+++ modules/borer-http4s/src/main/scala/http4s/BorerEntityJsonDecoder.scala
@@ -1,12 +1,12 @@
 package com.github.eikek.borer.compats.http4s
 
-import cats.effect.Async
+import cats.effect.Sync
 
 import io.bullet.borer.Decoder
 import org.http4s.EntityDecoder
 
 trait BorerEntityJsonDecoder:
-  given [F[_]: Async, A: Decoder]: EntityDecoder[F, A] =
+  given [F[_]: Sync, A: Decoder]: EntityDecoder[F, A] =
     BorerEntities.decodeEntityJson[F, A]
 
 object BorerEntityJsonDecoder extends BorerEntityJsonDecoder
