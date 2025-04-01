fa1256e94d33f1e7f55265e473792ec586638d28
eikek-scala-steward <eikek-scala-steward@users.noreply.github.com>
2025-04-01 06:17:23 +0000

    Update munit-cats-effect to 2.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val fs2 = "3.12.0"
     val http4s = "0.23.30"
     val munit = "1.1.0"
-    val munitCatsEffect = "2.0.0"
+    val munitCatsEffect = "2.1.0"
     val scribe = "3.15.0"
   }
 
